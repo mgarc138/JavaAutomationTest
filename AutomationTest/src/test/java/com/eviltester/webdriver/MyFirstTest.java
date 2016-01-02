94fce03ee56111bc1bc71a9acca2864738d6ac06
Marlon Garcia <marlongarcia16391@gmail.com>
2016-01-02 14:41:52 -0600

    Initial setup of the application
    One automated test is been done to test the setup

---
+++ AutomationTest/src/test/java/com/eviltester/webdriver/MyFirstTest.java
@@ -0,0 +1,28 @@
+package com.eviltester.webdriver;
+
+import org.junit.Test;
+import org.junit.Assert;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by marlongarcia on 1/2/16.
+ */
+
+public class MyFirstTest {
+    @Test
+    public void startWebDriver(){
+
+        WebDriver driver = new FirefoxDriver();
+
+        driver.navigate().to("http://seleniumsimplified.com");
+        Assert.assertTrue("title should start with Selenium Simplified", driver.getTitle().startsWith("Selenium Simplified"));
+
+        driver.close();
+        driver.quit();
+
+    }
+
+
+}
